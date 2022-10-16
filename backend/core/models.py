505a1df16cb4b57c6cf4152a8baaf98db84d3dca
llraekll <rakshith1@gmail.com>
2022-10-16 23:47:14 +0530

    First

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,10 @@
+from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User
+from sqlalchemy import null
 
 # Create your models here.
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    body = models.TextField()
