dff2b8d95195de35d62043484036b453bd635dd8
llraekll <rakshith1@gmail.com>
2022-10-17 14:11:50 +0530

    JWT

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,2 +1,24 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
+@api_view(['GET'])
+def hello(request):
+    routes = [ 'Hello human' ]
+    return Response(routes)
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
