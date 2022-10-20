382b9d2984e6e23f75737d4d4bae2f4ab8ba09eb
llraekll <rakshith1@gmail.com>
2022-10-20 19:29:23 +0530

    JWT FullStack

--- backend/core/models.py
+++ backend/core/models.py
@@ -7,3 +7,5 @@ from django.contrib.auth.models import User
 class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     body = models.TextField()
+
+
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,13 +1,17 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from .models import Note
+from .serializer import NoteSerializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 @api_view(['GET'])
 def hello(request):
-    routes = [ 'Hello human' ]
+    routes = ['Hello human']
     return Response(routes)
+
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -20,5 +24,15 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
         return token
 
+
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getnotes(request):
+    user = request.user
+    notes = user.note_set.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
